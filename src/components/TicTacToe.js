fef7c5f53cc7fe37d4fd32e109a0977a519068c1
Justin <zapfska@gmail.com>
2021-08-24 14:06:21 -0400

    Added Variables for the Functionalities of: Player Turn, Start Game and Rewind

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Board from "./Board";
 
 function TicTacToe() {
-  return <div></div>;
+  const turn = "It's O's Turn";
+  const start = <button>Start Game</button>;
+  const rewind = <button>Rewind</button>;
+  return (
+    <div>
+      <div className="ticTacToeBoard">
+        <Board></Board>
+      </div>
+      <div className="ticTacToeOptions">
+        <div>{turn}</div>
+        <div>{start}</div>
+        <div>{rewind}</div>
+      </div>
+    </div>
+  );
 }
 
 export default TicTacToe;
