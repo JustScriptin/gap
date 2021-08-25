0db804e2c0b739fc326199572d1b28bb5b7eb7ae
Justin <zapfska@gmail.com>
2021-08-24 22:14:31 -0400

    Learned how to listen for different events

--- src/App.test.js
+++ src/App.test.js
@@ -9,32 +9,28 @@ configure({ adapter: new Adapter() }); //////////
 //to console log the contents of a shallow wrapper you can use console.log(wrapper.debug())
 
 describe("Playing with Jest", () => {
- let wrapper;
+  let wrapper;
   beforeEach(() => {
     wrapper = shallow(<App />);
-  })
+  });
 
   test("render the title of counter", () => {
-    expect(wrapper.find("h1").text()).toContain("This is Tic Tac Toe");//toContain checks if it contains
+    expect(wrapper.find("h1").text()).toContain("This is Tic Tac Toe"); //toContain checks if it contains
 
     // render(<App />);
     // const linkElement = screen.getByText("This is Tic Tac Toe"); //gets app component as text
     // expect(linkElement).toBeInTheDocument(); //expects "linkElement" variable to be found in the document
   });
 
+  test("find string by id", () => {
+    expect(wrapper.find("#selectID").text()).toBe("This is Tic Tac Toe"); //selects by ID and expect exact value of "This is Tic Tac Toe"
+    //all returned outputs are strings so if you expect a number you have to put the number in quotes so it matches with the output
+  });
 
- 
-  test("render the title of counter", () => {
-    
-    expect(wrapper.find("#selectID").text()).toBe("This is Tic Tac Toe");//selects by ID and expect exact value of "This is Tic Tac Toe"
+  // describe("check for specific text", () => {
+  //   test("render the title of counter", () => {
+  //     const wrapper = shallow(<App />);
+  //     expect(wrapper.find("#selectID").text()).toBe("This is Tic Tac Toe");//selects by ID and expect exact value of "This is Tic Tac Toe"
+
+  //   });
 });
-  
-
-
-// describe("check for specific text", () => {
-//   test("render the title of counter", () => {
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.find("#selectID").text()).toBe("This is Tic Tac Toe");//selects by ID and expect exact value of "This is Tic Tac Toe"
-
-//   });
-// });
