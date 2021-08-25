2090ca1873fb0f8d42745b201dae1d059265e85f
Justin <zapfska@gmail.com>
2021-08-24 21:53:39 -0400

    Learned how to shorten boilerplate with beforeEach

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,32 @@ configure({ adapter: new Adapter() }); //////////
 //to console log the contents of a shallow wrapper you can use console.log(wrapper.debug())
 
 describe("Playing with Jest", () => {
+ let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
   test("render the title of counter", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find("h1").text()).toContain("This is Tic Tac Toe");
+    expect(wrapper.find("h1").text()).toContain("This is Tic Tac Toe");//toContain checks if it contains
 
     // render(<App />);
     // const linkElement = screen.getByText("This is Tic Tac Toe"); //gets app component as text
     // expect(linkElement).toBeInTheDocument(); //expects "linkElement" variable to be found in the document
   });
+
+
+ 
+  test("render the title of counter", () => {
+    
+    expect(wrapper.find("#selectID").text()).toBe("This is Tic Tac Toe");//selects by ID and expect exact value of "This is Tic Tac Toe"
 });
+  
+
+
+// describe("check for specific text", () => {
+//   test("render the title of counter", () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find("#selectID").text()).toBe("This is Tic Tac Toe");//selects by ID and expect exact value of "This is Tic Tac Toe"
+
+//   });
+// });
